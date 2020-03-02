ae55cc82e6ebe16cf3bba36e4f6f70e5cb4f4b24
ayrbox <srdx7@hotmail.com>
2020-03-02 18:43:10 +0000

    Read sheet information and data from specific sheet

--- packages/read-excel-file/index.js
+++ packages/read-excel-file/index.js
@@ -1,5 +1,8 @@
 const xlsx = require('read-excel-file/node');
 
+/**
+ * Read data from default Sheet
+ */
 const readExcelFile = async () => {
   try {
     const rows = await xlsx('./data.xlsx');
@@ -29,3 +32,30 @@ const readExcelFile = async () => {
 
 
 readExcelFile();
+
+
+/**
+ * Read Sheet Infomrations
+ */
+const readSheets = async () => {
+  const sheets = await xlsx('./data.xlsx', { getSheets: true });
+
+  sheets.forEach(sheetName => {
+    console.log('Sheet Name', sheetName);
+  });
+}
+
+readSheets();
+
+
+/**
+ * Read data from specified sheet (by Name or index)
+ */
+
+const readSheetData = async (sheetInfo) => {
+  const rows = await xlsx('./data.xlsx', { sheet: sheetInfo });
+  console.log(`Data from Sheet - "${sheetInfo}"`);
+  console.log(JSON.stringify(rows));
+}
+// readSheetData('Worksheet');
+readSheetData(1);
