a50f76c28bb1b611a0999cefbf68bc769e58d8a2
ayrbox <srdx7@hotmail.com>
2020-04-07 17:22:25 +0100

    Add docker-compose file for mongodb container and add mongoose

--- graphql-full-course-novice-to-expert/index.js
+++ graphql-full-course-novice-to-expert/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const graphHttp = require('express-graphql');
 
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb://localhost:27017/graphql_books';
+
+mongoose.connect(MONGO_URI);
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
 const schema = require('./schema/schema.js');
+
 
 const port = 4000;
 const app = express();
