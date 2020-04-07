758c3448b7dd8a037066dd2b249e9bf57b09801c
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2020-04-07 19:17:16 +0100

    Setup react client app with apollo

---
+++ graphql-full-course-novice-to-expert/client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+const BookList = ({}) => {
+  return (
+    <ListGroup>
+      <ListGroupItem>BooknName</ListGroupItem>
+      <ListGroupItem>BooknName</ListGroupItem>
+      <ListGroupItem>BooknName</ListGroupItem>
+      <ListGroupItem>BooknName</ListGroupItem>
+    </ListGroup>
+  );
+};
+
+export default BookList;
