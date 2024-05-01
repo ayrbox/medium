bcf0eb8113127e8bf2cdcf7dd9f8a2d9f8696d72
ayrbox <sabin.dangol@hotmail.com>
2024-05-01 21:10:24 +0100

    refactor msg func n assertion code.

--- learn-go-with-tests/hello-world/main.go
+++ learn-go-with-tests/hello-world/main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello "
+
 func GetMessage(name string) string {
-	return "Hello " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
